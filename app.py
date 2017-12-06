412aba79a3277fb6eb4dbdf0063a8036cef11fbb
Odselmaa <se11d002@gmail.com>
2017-12-06 21:42:01 +0300

    Testing

--- app.py
+++ app.py
@@ -1,8 +1,17 @@
-from flask import Flask, request
+from bson import ObjectId
+from flask import Flask, request, jsonify
+from flask import json
+from mongoengine import FieldDoesNotExist
+from werkzeug.exceptions import InternalServerError, BadRequest
 
-from models import db, Card
+from models import db, Card, Collection, User
+import requests
 
 app = Flask(__name__)
+POST = "POST"
+GET = "GET"
+DELETE = "DELETE"
+PUT = "PUT"
 #
 # app.config["MONGODB_SETTINGS"] = {"MONGODB_DB": "Portal",
 #                                   "MONGODB_HOST": "mongodb://admin_odko:WinniePooh@portalinternational-shard-00-00-3b6lw.mongodb.net:27017,"
@@ -22,17 +31,91 @@ app.config["MONGODB_PASSWORD"] = "WinniePooh8"
 db.init_app(app)
 
 
-@app.route('/card', methods=["POST", "GET", "DELETE", "PUT"])
-def hello_world():
-    if request.method == "GET":
+def add_card(cards):
+    cards_id = []
+    for item in cards:
+        card = Card(**item).save()
+        cards_id.append(card.pk)
+    return cards_id
+
+
+@app.route('/card', methods=[POST, GET, DELETE, PUT])
+def card():
+    if request.method == GET:
         cards = Card.objects.to_json()
         return cards
+    elif request.method == POST:
+        json = request.json
+        cards_id = add_card(json)
+        return jsonify(cards_id)
+
+
+@app.route('/collection', methods=[POST, GET, DELETE, PUT])
+def collection():
+    if request.method == GET:
+        collection = Collection.objects.to_json()
+        return collection
+    elif request.method == POST:
+        data = request.json
+
+        collection = data['collection']
+        collection = Collection(**collection)
+        cards = data['cards']
+        card_ids = add_card(cards)
+        collection.cards = card_ids
+        collection.save()
+
+        user_id = data["user_id"]
+        updated = User.objects(id=ObjectId(user_id)).update_one(push__collections=collection.pk)
+        return jsonify({"collection_id": str(collection.pk), "update":updated})
+
+
+@app.route('/user', methods=[POST, GET])
+def user():
+    if request.method == POST:
+        user_json = request.json
+        # user = User(**user_json)
+        return jsonify({"user_id": "bla"})
+
+
+def send_request(URL, method, json):
+    result = {}
+    if method == GET:
+        result = requests.get(URL, json=json).json()
+    elif method == POST:
+        result = requests.post(URL, json=json).json()
+    elif method == PUT:
+        result = requests.put(URL, json=json).json()
+    elif method == DELETE:
+        result = requests.delete(URL, json=json).json()
+    return result
+
+
+def str2objectid(id_list):
+    for i, item in enumerate(id_list):
+        id_list[i] = ObjectId(item)
+    return id_list
+
+
+@app.errorhandler(InternalServerError)
+def global_handler_500(e):
+    return jsonify({"error": "500", "message": str(e)}), 500
+
+
+@app.errorhandler(FieldDoesNotExist)
+def global_handler_file_doesnt_exist(e):
+    return jsonify({"error": 400, "message": str(e)})
+
+
+@app.errorhandler(BadRequest)
+def global_handler_bad_request(e):
+    return jsonify({"error": 401, "message": str(e)})
+
 
 
 @app.route('/')
 def index():
     return "Hey cutie"
-
 
 
 if __name__ == '__main__':
